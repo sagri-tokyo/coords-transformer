a4d79f8fe93ca3b4b3720b7ae7f30cd75f49438b
tsurutan <tsurutan.android@gmail.com>
2021-03-11 07:27:20 +0900

    feat: 🎸 First commit for coordinates-transformer

---
+++ src/location_converter.py
@@ -0,0 +1,22 @@
+from constants import L, ZOOM
+import math
+
+
+class LocationConverter:
+    @classmethod
+    def convert_lng_zoom_to_x(cls, long, zoom=ZOOM):
+        return round((2 ** (zoom + 7)) * ((long / 180) + 1))
+
+    @classmethod
+    def convert_lat_zoom_to_y(cls, lati, zoom=ZOOM):
+        return round(((2 ** (zoom + 7)) / math.pi) * ((-1 * math.atanh(math.sin((math.pi / 180) * lati))) + (math.atanh(math.sin((math.pi / 180) * L)))))
+
+    @classmethod
+    def convert_x_zoom_to_lng(cls, x, zoom=ZOOM):
+        return 180 * ((x / 2 ** (zoom + 7)) - 1)
+
+    @classmethod
+    def convert_y_to_lat(cls, y, zoom=ZOOM):
+        atanh_value = math.sin((math.pi / 180) * L)
+        tanh_value = -1 * (math.pi / (2 ** (zoom + 7))) * y + math.atanh(atanh_value)
+        return (180 / math.pi) * (math.asin(math.tanh(tanh_value)))
