7d9e02e01a5eeaf3f28ad6e57915a870243a06ec
moonorange <monoma632@gmail.com>
2021-03-24 16:28:09 +0900

    update: test to cover more cases

--- src/create_point_from_image_pixel_coords.py
+++ src/create_point_from_image_pixel_coords.py
@@ -1,12 +1,10 @@
 from src.point.factory import *
 from src.point.point import Point
-from typing import List
 
 
-def create_point_from_image_pixel_coords(point: List[int], ctr_pt: Point, size : int, zoom: int) -> Point:
+def create_point_from_image_pixel_coords(x_distance: int, y_distance: int, ctr_pt: Point, size : int, zoom: int) -> Point:
 	diff = int(size / 2)
 	origin_x = ctr_pt.x - diff
 	origin_y = ctr_pt.y - diff
-	x_distance, y_distance = point[0], point[1]
 	point_obj = create_from_x_y_zoom(x=origin_x + x_distance, y=origin_y + y_distance, zoom=zoom)
 	return point_obj
--- tests/test_create_point_from_centerpoint_size_zoom.py
+++ tests/test_create_point_from_centerpoint_size_zoom.py
@@ -1,9 +1,15 @@
 import pytest
 from src.constants import ZOOM, PIXEL_SIZE
 from src.create_point_from_image_pixel_coords import *
+from tests.conftest import SAMPLE_POINTS
 
-SAMPLE_PIXEL_COORDS = [256, 256]
+HALF_PIXEL = int(PIXEL_SIZE / 2)
+CTR_POINT = SAMPLE_POINTS[0]
+ORIGIN_X, ORIGIN_Y = CTR_POINT.x - HALF_PIXEL, CTR_POINT.y - HALF_PIXEL
+DIFFS = [{"x": 5, "y": 10}, {"x": 151, "y": 256}, {"x": 501, "y": 432}]
 
-def test_create_point_from_image_pixel_coords(sample_point):
-	point_obj = create_point_from_image_pixel_coords(SAMPLE_PIXEL_COORDS, sample_point, PIXEL_SIZE, ZOOM)
-	assert point_obj.__dict__ == pytest.approx(sample_point.__dict__)
+@pytest.mark.parametrize("diff", DIFFS)
+def test_create_point_from_image_pixel_coords(diff):
+	expected_point = create_from_x_y_zoom(ORIGIN_X + diff["x"], ORIGIN_Y + diff["y"], ZOOM)
+	point_obj = create_point_from_image_pixel_coords(diff["x"], diff["y"], CTR_POINT, PIXEL_SIZE, ZOOM)
+	assert point_obj.__dict__ == expected_point.__dict__
