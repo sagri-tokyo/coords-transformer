97227f83eb97fbb570b9c1c9e5b0037b65ff8d1a
moonorange <monoma632@gmail.com>
2021-04-14 17:41:37 +0900

    fix: test name, dir and docs

--- coords_transformer/point/service.py
+++ coords_transformer/point/service.py
@@ -5,13 +5,13 @@ from .point import Point
 def create_point_from_image_pixel_coords(x_distance: int, y_distance: int, ctr_pt: Point, size: int, zoom: int) -> Point:
     """
     Args:
-        x_distance: x pixel coordinate within an image.
-        y_distance: y pixel coordinate within an image.
-        ctr_pt: a Point object of the center point in an image.
+        x_distance: x pixel coordinate within the satellite image.
+        y_distance: y pixel coordinate within the satellite image.
+        ctr_pt: the Point object of the center point of the image.
         size: pixel image size.
-        zoom: zoom level of an image.
+        zoom: zoom level of the image.
     Returns:
-        a Point object
+        Point object
     """
     diff = int(size / 2)
     origin_x = ctr_pt.x - diff
