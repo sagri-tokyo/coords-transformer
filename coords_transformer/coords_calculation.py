578d4e568f548981dec5574de6e7138ce43ccfc3
tadanobu <public@tadanobu.com>
2021-09-16 17:40:42 +0900

    update: simplify coordinate calculation

--- coords_transformer/coords_calculation.py
+++ coords_transformer/coords_calculation.py
@@ -1,16 +1,21 @@
-from coords_transformer.constants import L
 import math
 
-# These calculations are based on the follwing link.
-# https://www.trail-note.net/tech/coordinate/
+u""" These calculations are based on the following link.
+https://www.trail-note.net/tech/coordinate/
+but replaced atanh(sin((π / 180) * L)) with π.
+
+This simplification is based on the following derivations:
+L = 85.05112878
+atan(sinh(π)) * 180/π = L + round_error
+atanh(sin((π / 180) * atan(sinh(π)) * 180/π)) = π
+"""
 
 def convert_lng_zoom_to_x(lng: float, zoom: int) -> int:
     return round((2 ** (zoom + 7)) * ((lng / 180) + 1))
 
 
 def convert_lat_zoom_to_y(lat: float, zoom: int) -> int:
-    atanh_value = math.sin((math.pi / 180) * L)
-    return round(((2 ** (zoom + 7)) / math.pi) * ((-1 * math.atanh(math.sin((math.pi / 180) * lat))) + (math.atanh(atanh_value))))
+    return round(((2 ** (zoom + 7)) / math.pi) * ((-1 * math.atanh(math.sin((math.pi / 180) * lat))) + math.pi))
 
 
 def convert_x_zoom_to_lng(x: int, zoom: int) -> float:
@@ -18,6 +23,5 @@ def convert_x_zoom_to_lng(x: int, zoom: int) -> float:
 
 
 def convert_y_zoom_to_lat(y: int, zoom: int) -> float:
-    atanh_value = math.sin((math.pi / 180) * L)
-    tanh_value = -1 * (math.pi / (2 ** (zoom + 7))) * y + math.atanh(atanh_value)
+    tanh_value = -1 * (math.pi / (2 ** (zoom + 7))) * y + math.pi
     return (180 / math.pi) * (math.asin(math.tanh(tanh_value)))
